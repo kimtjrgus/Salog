db67774e1bcfe96516e0064e38780e7302c817c4
kimtjrgus <sh95921@gmail.com>
2023-11-22 17:41:00 +0900

    initial setting

---
+++ server/src/main/java/com/codemouse/salog/SalogApplication.java
@@ -0,0 +1,13 @@
+package com.codemouse.salog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalogApplication.class, args);
+	}
+
+}
