c3f7610c86269147d1ab8606320b8d1bad98a8da
YujunSun0 <cons202@gmail.com>
2023-11-29 20:49:13 +0900

    feat : 회원가입 페이지 구현 & 로그인 페이지 소셜 로그인 목업

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,6 +5,7 @@ import Dashboard from "./pages/dashboard";
 import Diary from "./pages/diary";
 import History from "./pages/imcome_outgo";
 import Login from "./pages/login";
+import SignUp from "./pages/sign_up";
 
 function App() {
 	return (
@@ -16,6 +17,7 @@ function App() {
 					<Route path={"/diary"} element={<Diary />} />
 				</Route>
 				<Route path={"/login"} element={<Login />} />
+				<Route path={"/signup"} element={<SignUp />} />
 			</Routes>
 		</>
 	);
--- client/src/pages/login/index.tsx
+++ client/src/pages/login/index.tsx
@@ -1,7 +1,12 @@
 import styled from "styled-components";
+import { useState } from "react";
 import { SvgIcon } from "@mui/material";
 import VisibilityRoundedIcon from "@mui/icons-material/VisibilityRounded";
-import { useState } from "react";
+import VisibilityOffRoundedIcon from "@mui/icons-material/VisibilityOffRounded";
+import { ReactComponent as Kakao } from "../../assets/Kakao.svg";
+import { ReactComponent as Google } from "../../assets/Google.svg";
+import { ReactComponent as Naver } from "../../assets/Naver.svg";
+import { useNavigate } from "react-router-dom";
 
 interface userType {
 	email: string;
@@ -10,11 +15,17 @@ interface userType {
 
 const Login = () => {
 	const [values, setValues] = useState<userType>({ email: "", password: "" });
-	console.log(values);
+	const [isVisible, setIsVisible] = useState<boolean>(false);
+	const navigate = useNavigate();
 
+	// 이메일, 비밀번호가 변경될 때 상태를 저장하는 함수
 	const onChangeValues = (e: React.ChangeEvent<HTMLInputElement>) => {
-		const { type, value } = e.target;
-		setValues({ ...values, [type]: value });
+		const { name, value } = e.target;
+		setValues({ ...values, [name]: value });
+	};
+	// 비밀번호 표시/감추기 버튼 클릭 시 실행되는 함수
+	const onClickVisibleBtn = () => {
+		setIsVisible(!isVisible);
 	};
 
 	return (
@@ -23,21 +34,62 @@ const Login = () => {
 				<h2>로그인</h2>
 				<LoginDiv>
 					<Title>이메일</Title>
-					<Input type="email" onChange={onChangeValues} />
+					<Input type="email" name="email" onChange={onChangeValues} />
 					<Title>비밀번호</Title>
 					<PasswordLabel>
-						<Input type="password" onChange={onChangeValues} />
-						<button>
-							<SvgIcon
-								component={VisibilityRoundedIcon}
-								sx={{ stroke: "#ffffff", strokeWidth: 1 }}
-							/>
+						<Input
+							type={isVisible ? "text" : "password"}
+							name="password"
+							onChange={onChangeValues}
+						/>
+						<button onClick={onClickVisibleBtn}>
+							{isVisible ? (
+								<SvgIcon
+									component={VisibilityOffRoundedIcon}
+									sx={{ stroke: "#ffffff", strokeWidth: 1 }}
+								/>
+							) : (
+								<SvgIcon
+									component={VisibilityRoundedIcon}
+									sx={{ stroke: "#ffffff", strokeWidth: 1 }}
+								/>
+							)}
 						</button>
 					</PasswordLabel>
 					<SubmitBtn>
 						<p>로그인</p>
 					</SubmitBtn>
 				</LoginDiv>
+				<RedirectContainer>
+					<p>비밀번호를 잊으셨나요?</p>
+					<span>비밀번호 찾기</span>
+				</RedirectContainer>
+				<RedirectContainer>
+					<p>아직 회원이 아니신가요?</p>
+					<span
+						onClick={() => {
+							navigate("/signup");
+						}}
+					>
+						회원가입
+					</span>
+				</RedirectContainer>
+				<TitleWithLine>
+					<hr />
+					<span>또는</span>
+					<hr />
+				</TitleWithLine>
+				<OauthBtnContainer>
+					<OauthBtn>
+						<Kakao />
+					</OauthBtn>
+					<OauthBtn>
+						<Google />
+					</OauthBtn>
+					<OauthBtn>
+						<Naver />
+					</OauthBtn>
+				</OauthBtnContainer>
 			</LoginContainer>
 		</Container>
 	);
@@ -66,8 +118,7 @@ const LoginContainer = styled.div`
 
 const LoginDiv = styled.div`
 	width: 43.5rem;
-	height: 60vh;
-	margin-top: 5rem;
+	margin-top: 4rem;
 	display: flex;
 	flex-direction: column;
 
@@ -76,12 +127,12 @@ const LoginDiv = styled.div`
 	}
 `;
 
-const PasswordLabel = styled.label`
+export const PasswordLabel = styled.label`
 	position: relative;
 
 	button {
 		position: absolute;
-		top: 50%;
+		top: 47%;
 		right: 2rem;
 
 		svg {
@@ -90,7 +141,7 @@ const PasswordLabel = styled.label`
 	}
 `;
 
-const Input = styled.input.attrs({ required: true })`
+export const Input = styled.input.attrs({ required: true })`
 	width: 100%;
 	height: 4.3rem;
 	margin-top: 1.5rem;
@@ -105,10 +156,11 @@ const Title = styled.p`
 	margin-top: 2rem;
 `;
 
-const SubmitBtn = styled.button`
+export const SubmitBtn = styled.button`
 	width: 100%;
 	height: 4.3rem;
 	margin-top: 5rem;
+	margin-bottom: 2rem;
 	border-radius: 0.8rem;
 	background-color: ${(props) => props.theme.COLORS.LIGHT_BLUE};
 
@@ -117,6 +169,57 @@ const SubmitBtn = styled.button`
 		font-weight: 500;
 		font-size: 1.6rem;
 	}
+`;
+
+const RedirectContainer = styled.div`
+	margin-top: 1rem;
+	width: 19rem;
+	text-align: center;
+	justify-content: space-between;
+	font-size: 1.2rem;
+
+	p {
+		display: inline;
+		color: ${(props) => props.theme.COLORS.GRAY_500};
+	}
+
+	span {
+		color: ${(props) => props.theme.COLORS.LIGHT_BLUE};
+		margin-left: 0.5rem;
+		cursor: pointer;
+	}
+`;
+
+const TitleWithLine = styled.div`
+	width: 43.5rem;
+	margin-top: 1.5rem;
+	font-size: 1.2rem;
+	display: flex;
+	align-items: center;
+
+	hr {
+		flex: auto;
+		height: 1px;
+		border: 0;
+		background-color: ${(props) => props.theme.COLORS.GRAY_300};
+	}
+
+	span {
+		padding: 0 10px;
+		color: ${(props) => props.theme.COLORS.GRAY_500};
+	}
+`;
+
+const OauthBtnContainer = styled.div`
+	display: flex;
+	margin-top: 1.5rem;
+`;
+
+const OauthBtn = styled.button`
+	width: 40px;
+	height: 40px;
+	border: none;
+	margin: 0 1.5rem;
 `;
 
 export default Login;
