baa68e10014f7faea082e166b21d2ec165dbad1e
jerry <021skyfall@gmail.com>
2023-12-08 16:01:37 +0900

    feat : 회원 조회(응답X), 회원 삭제 구현 / 서비스 로직에 존재하는 회원인지 체크 로직 구현

--- server/src/main/java/com/codemouse/salog/members/controller/MemberController.java
+++ server/src/main/java/com/codemouse/salog/members/controller/MemberController.java
@@ -1,6 +1,9 @@
 package com.codemouse.salog.members.controller;
 
 import com.codemouse.salog.members.dto.MemberDto;
+import com.codemouse.salog.members.entity.Member;
+import com.codemouse.salog.members.service.MemberService;
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,8 +15,11 @@ import javax.validation.Valid;
 @RestController
 @RequestMapping("/members")
 @Validated
+@AllArgsConstructor
 @Slf4j
 public class MemberController {
+    private final MemberService memberService;
+
     @PostMapping("/signup")
     @ResponseStatus(HttpStatus.CREATED)
     public void SignupMember(@Valid @RequestBody MemberDto.Post requestBody) {
@@ -22,18 +28,20 @@ public class MemberController {
 
     @PatchMapping("/update")
     @ResponseStatus(HttpStatus.OK)
-    public void SignupMember(@Valid @RequestBody MemberDto.Patch requestBody) {
+    public void updateMember(@Valid @RequestBody MemberDto.Patch requestBody) {
 
     }
 
     @GetMapping("/get")
-    public ResponseEntity SignupMember(@Valid @RequestBody MemberDto.Response requestBody) {
+    public ResponseEntity getMember(@RequestHeader(name = "Authorization") String token) {
+        Member member = memberService.findMember(token);
+
         return null;
     }
 
     @DeleteMapping("/leaveid")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void SignupMember() {
-
+    public void deleteMember(@RequestHeader(name = "Authorization") String token) {
+        memberService.deleteMember(token);
     }
 }
--- server/src/main/java/com/codemouse/salog/members/service/MemberService.java
+++ server/src/main/java/com/codemouse/salog/members/service/MemberService.java
@@ -1,14 +1,23 @@
 package com.codemouse.salog.members.service;
 
+import com.codemouse.salog.auth.jwt.JwtTokenizer;
+import com.codemouse.salog.exception.BusinessLogicException;
+import com.codemouse.salog.exception.ExceptionCode;
 import com.codemouse.salog.members.entity.Member;
+import com.codemouse.salog.members.repository.MemberRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Transactional
 @Service
 @AllArgsConstructor
 public class MemberService {
+    private final JwtTokenizer jwtTokenizer;
+    private final MemberRepository memberRepository;
+
     public Member createMember(Member member) {
         return null;
     }
@@ -17,11 +26,24 @@ public class MemberService {
         return null;
     }
 
-    public void deleteMember(long memberId) {
+    public Member findMember(String token) {
+        Member findMember = findVerifiedMember(jwtTokenizer.getMemberId(token));
 
+        if(findMember.getStatus().equals(Member.Status.MEMBER_QUIT)) {
+            throw new BusinessLogicException(ExceptionCode.MEMBER_NOT_FOUND);
+        }
+
+        return findMember;
     }
 
-    public Member findMember(long memberId) {
-        return null;
+    public void deleteMember(String token) {
+        memberRepository.deleteById(jwtTokenizer.getMemberId(token));
+    }
+
+    public Member findVerifiedMember(long memberId){
+        Optional<Member> optionalMember =
+                memberRepository.findById(memberId);
+        return optionalMember.orElseThrow(() ->
+                new BusinessLogicException(ExceptionCode.MEMBER_NOT_FOUND));
     }
 }
