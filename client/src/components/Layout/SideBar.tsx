c7571b4ba1a8d10af15e54be3ef8009c888475b6
YujunSun0 <cons202@gmail.com>
2023-12-21 23:14:05 +0900

    feat : 로그아웃 구현

--- client/src/components/Layout/SideBar.tsx
+++ client/src/components/Layout/SideBar.tsx
@@ -9,84 +9,116 @@ import StickyNote2OutlinedIcon from "@mui/icons-material/StickyNote2Outlined";
 import ContactSupportOutlinedIcon from "@mui/icons-material/ContactSupportOutlined";
 import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
 import ExitToAppRoundedIcon from "@mui/icons-material/ExitToAppRounded";
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
+import Modal from "./Modal";
+import { useState } from "react";
+import { removeCookie } from "src/utils/cookie";
 
 export const SideBar = () => {
+	const [isOpen, setIsOpen] = useState(false);
+	const navigate = useNavigate();
+
+	const onclickLogout = () => {
+		setIsOpen(true);
+	};
+
+	const onClickCloseBtn = () => {
+		removeCookie("accessToken", { path: "/" });
+		removeCookie("refreshToken", { path: "/" });
+		navigate("/login", { replace: true });
+	};
+
 	return (
-		<Container>
-			<NavTitle>ACCOUNT BOOK</NavTitle>
-			<Lists>
-				<NavStyle to="/">
-					<SvgIcon
-						component={GridViewIcon}
-						sx={{ stroke: "#ffffff", strokeWidth: 0.5 }}
-					/>
-					<ListTitle>대시보드</ListTitle>
-				</NavStyle>
-				<NavStyle to="/history">
-					<SvgIcon
-						component={MenuBookIcon}
-						sx={{ stroke: "#ffffff", strokeWidth: 0.5 }}
-					/>
-					<ListTitle>지출 · 수입</ListTitle>
-				</NavStyle>
-				<NavStyle to="/monthRatio">
-					<SvgIcon
-						component={SignalCellularAltIcon}
-						sx={{ stroke: "#ffffff", strokeWidth: 0.5 }}
-					/>
-					<ListTitle>분석</ListTitle>
-				</NavStyle>
-				<NavStyle to="/budget">
-					<SvgIcon
-						component={PriceChangeOutlinedIcon}
-						sx={{ stroke: "#ffffff", strokeWidth: 0.5 }}
-					/>
-					<ListTitle>예산</ListTitle>
-				</NavStyle>
-				<NavStyle to="/fixed_account">
-					<SvgIcon
-						component={AttachMoneyOutlinedIcon}
-						sx={{ stroke: "#ffffff", strokeWidth: 0.5 }}
-					/>
-					<ListTitle>고정 지출 · 수입</ListTitle>
-				</NavStyle>
-			</Lists>
-			<NavTitle>DIARY</NavTitle>
-			<Lists>
-				<NavStyle to="/diary">
-					<SvgIcon
-						component={StickyNote2OutlinedIcon}
-						sx={{ stroke: "#ffffff", strokeWidth: 0.5 }}
-					/>
-					<ListTitle>일기</ListTitle>
-				</NavStyle>
-			</Lists>
-			<NavTitle>ACCOUNT</NavTitle>
-			<Lists>
-				<NavStyle to="/inquiry">
-					<SvgIcon
-						component={ContactSupportOutlinedIcon}
-						sx={{ stroke: "#ffffff", strokeWidth: 0.5 }}
-					/>
-					<ListTitle>문의</ListTitle>
-				</NavStyle>
-				<NavStyle to="/setting">
-					<SvgIcon
-						component={SettingsOutlinedIcon}
-						sx={{ stroke: "#ffffff", strokeWidth: 0.5 }}
-					/>
-					<ListTitle>설정</ListTitle>
-				</NavStyle>
-				<NavStyle to="/setting">
-					<SvgIcon
-						component={ExitToAppRoundedIcon}
-						sx={{ stroke: "#ffffff", strokeWidth: 0.5 }}
-					/>
-					<ListTitle>로그아웃</ListTitle>
-				</NavStyle>
-			</Lists>
-		</Container>
+		<>
+			<Container>
+				<NavTitle>ACCOUNT BOOK</NavTitle>
+				<Lists>
+					<NavStyle to="/">
+						<SvgIcon
+							component={GridViewIcon}
+							sx={{ stroke: "#ffffff", strokeWidth: 0.5 }}
+						/>
+						<ListTitle>대시보드</ListTitle>
+					</NavStyle>
+					<NavStyle to="/history">
+						<SvgIcon
+							component={MenuBookIcon}
+							sx={{ stroke: "#ffffff", strokeWidth: 0.5 }}
+						/>
+						<ListTitle>지출 · 수입</ListTitle>
+					</NavStyle>
+					<NavStyle to="/monthRatio">
+						<SvgIcon
+							component={SignalCellularAltIcon}
+							sx={{ stroke: "#ffffff", strokeWidth: 0.5 }}
+						/>
+						<ListTitle>분석</ListTitle>
+					</NavStyle>
+					<NavStyle to="/budget">
+						<SvgIcon
+							component={PriceChangeOutlinedIcon}
+							sx={{ stroke: "#ffffff", strokeWidth: 0.5 }}
+						/>
+						<ListTitle>예산</ListTitle>
+					</NavStyle>
+					<NavStyle to="/fixed_account">
+						<SvgIcon
+							component={AttachMoneyOutlinedIcon}
+							sx={{ stroke: "#ffffff", strokeWidth: 0.5 }}
+						/>
+						<ListTitle>고정 지출 · 수입</ListTitle>
+					</NavStyle>
+				</Lists>
+				<NavTitle>DIARY</NavTitle>
+				<Lists>
+					<NavStyle to="/diary">
+						<SvgIcon
+							component={StickyNote2OutlinedIcon}
+							sx={{ stroke: "#ffffff", strokeWidth: 0.5 }}
+						/>
+						<ListTitle>일기</ListTitle>
+					</NavStyle>
+				</Lists>
+				<NavTitle>ACCOUNT</NavTitle>
+				<Lists>
+					<NavStyle to="/inquiry">
+						<SvgIcon
+							component={ContactSupportOutlinedIcon}
+							sx={{ stroke: "#ffffff", strokeWidth: 0.5 }}
+						/>
+						<ListTitle>문의</ListTitle>
+					</NavStyle>
+					<NavStyle to="/setting">
+						<SvgIcon
+							component={SettingsOutlinedIcon}
+							sx={{ stroke: "#ffffff", strokeWidth: 0.5 }}
+						/>
+						<ListTitle>설정</ListTitle>
+					</NavStyle>
+					<LogoutDiv onClick={onclickLogout}>
+						<SvgIcon
+							component={ExitToAppRoundedIcon}
+							sx={{ stroke: "#ffffff", strokeWidth: 0.5 }}
+						/>
+						<ListTitle>로그아웃</ListTitle>
+					</LogoutDiv>
+				</Lists>
+			</Container>
+			<Modal
+				state={isOpen}
+				setState={setIsOpen}
+				msgTitle={"로그아웃 하시겠습니까?"}
+			>
+				<button
+					onClick={() => {
+						setIsOpen((prev) => !prev);
+					}}
+				>
+					취소
+				</button>
+				<button onClick={onClickCloseBtn}>확인</button>
+			</Modal>
+		</>
 	);
 };
 
@@ -117,6 +149,31 @@ const Lists = styled.div`
 `;
 
 const NavStyle = styled(NavLink)`
+	display: flex;
+	align-items: center;
+	padding: 0.5rem 1.3rem;
+	cursor: pointer;
+
+	> svg {
+		font-size: 2.1rem;
+		color: ${(props) => props.theme.COLORS.GRAY_600};
+		border: 1px;
+	}
+
+	&:hover {
+		background-color: #f0f3fd;
+	}
+
+	&.active {
+		background-color: #e2e8ff;
+		> svg,
+		p {
+			color: ${(props) => props.theme.COLORS.LIGHT_BLUE};
+		}
+	}
+`;
+
+const LogoutDiv = styled.div`
 	display: flex;
 	align-items: center;
 	padding: 0.5rem 1.3rem;
