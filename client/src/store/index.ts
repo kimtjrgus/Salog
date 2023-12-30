229f6990c51dd3e98099ead179d62ed9af0057cf
YujunSun0 <cons202@gmail.com>
2023-12-30 11:32:11 +0900

    redux-toolkit 설정

--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -1,9 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
+import ledgerSlice from "./slices/ledger";
 // import type { RootState, AppDispatch } from './store'
 
 // 리듀서들을 바인딩하는 저장소
 export const store = configureStore({
-	reducer: {},
+	reducer: {
+		monthlyOutgo: ledgerSlice,
+	},
 });
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
