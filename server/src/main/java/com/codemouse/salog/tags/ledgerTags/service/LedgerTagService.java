02339352a911ea28eb9d5af5e1dd5f2a7b53b284
jerry <021skyfall@gmail.com>
2024-01-04 18:56:44 +0900

    feat : LedgerTag 기본 구조 작성

--- server/src/main/java/com/codemouse/salog/tags/ledgerTags/service/LedgerTagService.java
+++ server/src/main/java/com/codemouse/salog/tags/ledgerTags/service/LedgerTagService.java
@@ -8,5 +8,9 @@ import org.springframework.stereotype.Service;
 @AllArgsConstructor
 @Slf4j
 public class LedgerTagService {
-    // todo 2024-01 전체조회는 없고 수입, 지출 조회시 불려 나가기만 하면 됨
+    /* todo 2024-01 전체조회는 없고 수입, 지출 조회시 불려 나가기만 하면 됨
+       태그 저장은 수입, 지출 상관없이 "저장 기능"만 있으면 될듯함 -> 어차피 수입,지출 id로 분류됨
+       다만 수입, 지출 부를때 같이 조회되게끔 하는 로직을 짜야함 -> n+1 문제가 있을 수도
+       그리고 회원 로그아웃 파악은 여기서는 안해도될 듯함 -> 어차피 수입, 지출에서 먼저하기 떄문
+    */
 }
