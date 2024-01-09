5c5952ef616a28dffb9354cae2cffb0137fc9e22
jerry <021skyfall@gmail.com>
2024-01-09 16:50:26 +0900

    feat : 고정 수입 기능 구현 완료

--- server/src/main/java/com/codemouse/salog/ledger/fixedIncome/dto/FixedIncomeDto.java
+++ server/src/main/java/com/codemouse/salog/ledger/fixedIncome/dto/FixedIncomeDto.java
@@ -1,4 +1,33 @@
 package com.codemouse.salog.ledger.fixedIncome.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.time.LocalDate;
+
 public class FixedIncomeDto {
+    @AllArgsConstructor
+    @Getter
+    public static class Post {
+        private int money;
+        private String incomeName;
+        private LocalDate date;
+    }
+
+    @AllArgsConstructor
+    @Getter
+    public static class Patch {
+        private int money;
+        private String incomeName;
+        private LocalDate date;
+    }
+
+    @AllArgsConstructor
+    @Getter
+    public static class Response {
+        private long fixedIncomeId;
+        private int money;
+        private String incomeName;
+        private LocalDate date;
+    }
 }
