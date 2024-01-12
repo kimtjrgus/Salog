c47947a1c628b5b4916e6bd897f4ac1a40591778
YujunSun0 <cons202@gmail.com>
2024-01-12 12:08:20 +0900

    feat : redux-persist 세팅 및 유저 상태 설정

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,7 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { Provider } from "react-redux";
-import { store } from "./store/index";
+import store from "./store";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
 import GlobalStyle from "./styles/GlobalStyle";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import { BrowserRouter } from "react-router-dom";
@@ -10,19 +12,23 @@ import { ThemeProvider } from "styled-components";
 import Theme from "./styles/Theme";
 import RefreshToken from "./utils/refreshToken";
 
+export const persistor = persistStore(store);
+
 const root = ReactDOM.createRoot(
 	document.getElementById("root") as HTMLElement,
 );
 root.render(
 	<React.StrictMode>
 		<Provider store={store}>
-			<BrowserRouter>
-				<RefreshToken />
-				<ThemeProvider theme={Theme}>
-					<GlobalStyle />
-					<App />
-				</ThemeProvider>
-			</BrowserRouter>
+			<PersistGate loading={null} persistor={persistor}>
+				<BrowserRouter>
+					<RefreshToken />
+					<ThemeProvider theme={Theme}>
+						<GlobalStyle />
+						<App />
+					</ThemeProvider>
+				</BrowserRouter>
+			</PersistGate>
 		</Provider>
 	</React.StrictMode>,
 );
--- client/src/store/slices/userSlice.ts
+++ client/src/store/slices/userSlice.ts
@@ -4,10 +4,16 @@ const userSlice = createSlice({
 	name: "user",
 	initialState: {
 		isLoggedIn: false,
+		emailAlarm: false,
+		homeAlarm: false,
+		createdAt: "",
 	},
 	reducers: {
-		login: (state) => {
+		login: (state, action) => {
 			state.isLoggedIn = true;
+			state.emailAlarm = action.payload.emailAlarm;
+			state.homeAlarm = action.payload.homeAlarm;
+			state.createdAt = action.payload.createdAt;
 		},
 		logout: (state) => {
 			state.isLoggedIn = false;
