6d594d35abfafdac87b0d4089cad0d3b650b6f19
kimtjrgus <sh95921@gmail.com>
2024-01-15 13:43:09 +0900

    feat : 지출 결제수단 항목 추가

--- server/src/main/java/com/codemouse/salog/ledger/outgo/dto/OutgoDto.java
+++ server/src/main/java/com/codemouse/salog/ledger/outgo/dto/OutgoDto.java
@@ -16,15 +16,23 @@ public class OutgoDto {
                 message = "date가 유효하지 않습니다. 'yyyy-MM-dd' 형식이 필요합니다")
         @NotBlank(message = "date가 비어있습니다.")
         private String date;
-        @Size(max = 15, message = "outgoName이 15글자이내여야 합니다.")
-        private String outgoName;
+
         @NotNull(message = "money가 비어있습니다.")
         private Long money;
+
+        @Size(max = 15, message = "outgoName이 15글자이내여야 합니다.")
+        private String outgoName;
+
+        private String payment;
+
         @Size(max = 20, message = "memo는 20자이내여야 합니다.")
         private String memo;
+
         @Pattern(regexp = "^\\S{1,10}$", message = "outgoTag는 공백 없이 1~10글자 사이여야 합니다.")
         private String outgoTag;
+
         private String receiptImg;
+
         @NotNull(message = "wasteList가 비어있습니다.")
         private Boolean wasteList;
     }
