463d420d41e7e38b8ab511a7fed76b5db20e5249
jerry <021skyfall@gmail.com>
2024-01-15 15:56:03 +0900

    ref : 회원, 태그 동시 조회 매퍼 수정

--- server/src/main/java/com/codemouse/salog/members/mapper/MemberMapper.java
+++ server/src/main/java/com/codemouse/salog/members/mapper/MemberMapper.java
@@ -4,43 +4,40 @@ import com.codemouse.salog.members.dto.MemberDto;
 import com.codemouse.salog.members.entity.Member;
 import com.codemouse.salog.tags.ledgerTags.dto.LedgerTagDto;
 import com.codemouse.salog.tags.ledgerTags.entity.LedgerTag;
+import com.codemouse.salog.tags.ledgerTags.mapper.LedgerTagMapper;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 import org.mapstruct.ReportingPolicy;
+import org.mapstruct.factory.Mappers;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
-@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE, uses = LedgerTagMapper.class)
 public interface MemberMapper {
+
     Member memberPostDtoToMember(MemberDto.Post requestBody);
+
     Member memberPatchDtoToMember(MemberDto.Patch requestBody);
-    default MemberDto.Response memberToMemberResponseDto(Member member) {
-        MemberDto.Response response = new MemberDto.Response();
 
-        response.setMemberId(member.getMemberId());
-        response.setEmailAlarm(member.isEmailAlarm());
-        response.setHomeAlarm(member.isHomeAlarm());
-        response.setCreatedAt(member.getCreatedAt());
+    @Mapping(source = "member", target = "incomeTags", qualifiedByName = "toIncomeTagList")
+    @Mapping(source = "member", target = "outgoTags", qualifiedByName = "toOutgoTagList")
+    MemberDto.Response memberToMemberResponseDto(Member member);
 
-        List<LedgerTagDto.Response> incomeTags = member.getLedgerTags().stream()
+    @Named("toIncomeTagList")
+    default List<LedgerTagDto.Response> toIncomeTagList(Member member) {
+        return member.getLedgerTags().stream()
                 .filter(tag -> tag.getCategory() == LedgerTag.Group.INCOME)
-                .map(this::ledgerTagToLedgerTagResponseDto)
+                .map(tag -> Mappers.getMapper(LedgerTagMapper.class).ledgerTagToLedgerTagResponseDto(tag))
                 .collect(Collectors.toList());
-        response.setIncomeTags(incomeTags);
-
-        List<LedgerTagDto.Response> outgoTags = member.getLedgerTags().stream()
-                .filter(tag -> tag.getCategory() == LedgerTag.Group.OUTGO)
-                .map(this::ledgerTagToLedgerTagResponseDto)
-                .collect(Collectors.toList());
-        response.setOutgoTags(outgoTags);
-
-        return response;
     }
 
-    default LedgerTagDto.Response ledgerTagToLedgerTagResponseDto(LedgerTag ledgerTag) {
-        LedgerTagDto.Response response = new LedgerTagDto.Response();
-        response.setLedgerTagId(ledgerTag.getLedgerTagId());
-        response.setTagName(ledgerTag.getTagName());
-        return response;
+    @Named("toOutgoTagList")
+    default List<LedgerTagDto.Response> toOutgoTagList(Member member) {
+        return member.getLedgerTags().stream()
+                .filter(tag -> tag.getCategory() == LedgerTag.Group.OUTGO)
+                .map(tag -> Mappers.getMapper(LedgerTagMapper.class).ledgerTagToLedgerTagResponseDto(tag))
+                .collect(Collectors.toList());
     }
 }
