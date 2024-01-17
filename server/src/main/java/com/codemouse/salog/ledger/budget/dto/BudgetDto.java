2e30fb9beda801a6675aa4305f20218aec006607
jerry <021skyfall@gmail.com>
2024-01-17 17:32:24 +0900

    feat : 월별 예산 기능 구현 완료 v1 / 월별 지출 합계 구현 시 추가 예정

---
+++ server/src/main/java/com/codemouse/salog/ledger/budget/dto/BudgetDto.java
@@ -0,0 +1,34 @@
+package com.codemouse.salog.ledger.budget.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+public class BudgetDto {
+    @AllArgsConstructor
+    @Getter
+    public static class Post {
+        private LocalDate date;
+        private long budget;
+    }
+
+    @AllArgsConstructor
+    @Getter
+    public static class Patch {
+        private LocalDate date;
+        private long budget;
+    }
+
+    @AllArgsConstructor
+    @Getter
+    @Setter
+    public static class Response {
+        private long budgetId;
+        private LocalDate date;
+        private long budget;
+        private int totalOutgo; // 지출 월별 합계 계산 (비교를 위함)
+        private int dayRemain;
+    }
+}
