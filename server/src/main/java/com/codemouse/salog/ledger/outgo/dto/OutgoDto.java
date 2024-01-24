4e4b1497e4be6c8405c6609a487e5df34b888aae
kimtjrgus <sh95921@gmail.com>
2024-01-24 17:38:12 +0900

    feat : outgo, fixedOutgo 유효성 검증 및 타입변경

--- server/src/main/java/com/codemouse/salog/ledger/outgo/dto/OutgoDto.java
+++ server/src/main/java/com/codemouse/salog/ledger/outgo/dto/OutgoDto.java
@@ -13,27 +13,16 @@ public class OutgoDto {
     @AllArgsConstructor
     @Getter
     public static class Post {
-        @Pattern(regexp = "^\\d{4}-(0[1-9]|1[0-2])-(0[1-9]|1[0-2]|2[0-8]|(29|30)(?!-02)|(31)(?<!-02|-04|-06|-09|-11))$",
-                message = "date가 유효하지 않습니다. 'yyyy-MM-dd' 형식이 필요합니다")
-        @NotBlank(message = "date가 비어있습니다.")
-        private String date;
-
-        @NotNull(message = "money가 비어있습니다.")
-        private Long money;
-
+        private LocalDate date;
+        private int money;
         @Size(max = 15, message = "outgoName이 15글자이내여야 합니다.")
         private String outgoName;
-
         private String payment;
-
         @Size(max = 20, message = "memo는 20자이내여야 합니다.")
         private String memo;
-
         @Pattern(regexp = "^\\S{1,10}$", message = "outgoTag는 공백 없이 1~10글자 사이여야 합니다.")
         private String outgoTag;
-
         private String receiptImg;
-
         @NotNull(message = "wasteList가 비어있습니다.")
         private Boolean wasteList;
     }
@@ -41,10 +30,8 @@ public class OutgoDto {
     @AllArgsConstructor
     @Getter
     public static class Patch {
-        @Pattern(regexp = "^\\d{4}-(0[1-9]|1[0-2])-(0[1-9]|1[0-2]|2[0-8]|(29|30)(?!-02)|(31)(?<!-02|-04|-06|-09|-11))$",
-                message = "date가 유효하지 않습니다. 'yyyy-MM-dd' 형식이 필요합니다")
-        private String date;
-        private Long money;
+        private LocalDate date;
+        private int money;
         @Size(max = 15, message = "outgoName이 15글자이내여야 합니다.")
         private String outgoName;
         private String payment;
@@ -61,7 +48,7 @@ public class OutgoDto {
     public static class Response {
         private long outgoId;
         private LocalDate date;
-        private long money;
+        private int money;
         private String outgoName;
         private String payment;
         private String memo;
