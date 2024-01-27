9041d7ad14f0a8d56a0226749c4bbf02e0da5d4f
YujunSun0 <cons202@gmail.com>
2024-01-27 20:40:10 +0900

    feat : 분석 페이지 중 일부 컴포넌트인 커스텀 pieChart 추가

---
+++ client/src/pages/monthRadio/CustomTooltip.tsx
@@ -0,0 +1,32 @@
+import { styled } from "styled-components";
+
+const CustomTooltip = ({ active, payload }: any) => {
+	if (active && payload?.length) {
+		return (
+			<Container>
+				<p className="label">{`${new Date(
+					payload[0].payload.date,
+				).getDate()}일 : ${payload[0].value}원`}</p>
+			</Container>
+		);
+	}
+
+	return null;
+};
+
+export default CustomTooltip;
+
+/* styled-components */
+
+const Container = styled.div`
+	width: fit-content;
+	height: 28px;
+	padding: 8px;
+	background-color: #fff;
+	opacity: 0.85;
+	border: 1px solid gray;
+	border-radius: 5px;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+`;
