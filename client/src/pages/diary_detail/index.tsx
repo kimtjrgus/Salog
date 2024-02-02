c17f753e5ff88449c9a1afd8b575c318df8363ed
YujunSun0 <cons202@gmail.com>
2024-02-02 15:21:58 +0900

    feat : 일기 조회시 지출,수입 내역 추가

--- client/src/pages/diary_detail/index.tsx
+++ client/src/pages/diary_detail/index.tsx
@@ -10,6 +10,7 @@ import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { api } from "src/utils/refreshToken";
 import Modal from "src/components/Layout/Modal";
+import { type incomeType, type outgoType } from "../income_outgo";
 
 interface detailType {
 	diaryId: number;
@@ -27,11 +28,36 @@ interface TagList {
 }
 
 const DiaryDetail = () => {
-	const [diary, setDiary] = useState<detailType | null>(null);
+	const [diary, setDiary] = useState<detailType>({
+		diaryId: 0,
+		date: "",
+		title: "",
+		body: "",
+		img: "",
+		tagList: [],
+	});
+	const [outgo, setOutgo] = useState<outgoType[]>([]);
+	const [income, setIncome] = useState<incomeType[]>([]);
 	const [isOpen, setIsOpen] = useState<boolean>(false);
 
 	const id = useParams().id;
 	const navigate = useNavigate();
+
+	const sumOfOutgo = () => {
+		let sum = 0;
+		outgo.forEach((el) => {
+			sum += el.money;
+		});
+		return sum;
+	};
+
+	const sumOfIncome = () => {
+		let sum = 0;
+		income.forEach((el) => {
+			sum += el.money;
+		});
+		return sum;
+	};
 
 	const onClickBackBtn = () => {
 		navigate("/diary");
@@ -47,7 +73,7 @@ const DiaryDetail = () => {
 
 	const onClickCloseBtn = () => {
 		api
-			.delete(`/diary/${id}/delete`)
+			.delete(`/diary/delete/${id}`)
 			.then(() => {
 				navigate("/diary");
 			})
@@ -61,6 +87,22 @@ const DiaryDetail = () => {
 			.get(`/diary/${id}`)
 			.then((res) => {
 				setDiary(res.data.data);
+				api
+					.get(`/outgo?page=1&size=15&date=${res.data.data.date}`)
+					.then((res) => {
+						setOutgo(res.data.data);
+					})
+					.catch((error) => {
+						console.log(error);
+					});
+				api
+					.get(`/income?page=1&size=15&date=${res.data.data.date}`)
+					.then((res) => {
+						setIncome(res.data.data);
+					})
+					.catch((error) => {
+						console.log(error);
+					});
 			})
 			.catch((error) => {
 				console.log(error);
@@ -112,7 +154,69 @@ const DiaryDetail = () => {
 						<ReactQuill theme="snow" value={diary?.body} readOnly={true} />
 					</Quill>
 				</DetailContainer>
-				<BookContainer>1</BookContainer>
+				<BookContainer>
+					<h3>작성한 가계부</h3>
+					<div className="outgo__lists">
+						<h5>{`지출 : ${outgo.length}건`}</h5>
+						<div className="lists__header">
+							<p>카테고리</p>
+							<p>거래처</p>
+							<p>금액</p>
+						</div>
+						<ul className="lists">
+							{outgo.length === 0 ? (
+								<p className="null__p">지출 내역이 존재하지 않습니다.</p>
+							) : (
+								outgo.map((el) => {
+									return (
+										<li className="list" key={el.outgoId}>
+											<p>{el.outgoTag.tagName}</p>
+											<div
+												className={`${
+													el.outgoName.length > 10 ? "over__div" : "under__div"
+												}`}
+											>
+												<p>{el.outgoName}</p>
+											</div>
+											<p>{el.money.toLocaleString()}원</p>
+										</li>
+									);
+								})
+							)}
+						</ul>
+						<p className="bottom__p">{`총 ${sumOfOutgo().toLocaleString()}원`}</p>
+					</div>
+					<div className="outgo__lists income">
+						<h5>{`수입 : ${income.length}건`}</h5>
+						<div className="lists__header">
+							<p>카테고리</p>
+							<p>거래처</p>
+							<p>금액</p>
+						</div>
+						<ul className="lists">
+							{income.length === 0 ? (
+								<p className="null__p">수입 내역이 존재하지 않습니다.</p>
+							) : (
+								income.map((el) => {
+									return (
+										<li className="list" key={el.incomeId}>
+											<p>{el.incomeTag.tagName}</p>
+											<div
+												className={`${
+													el.incomeName.length > 10 ? "over__div" : "under__div"
+												}`}
+											>
+												<p>{el.incomeName}</p>
+											</div>
+											<p>{el.money.toLocaleString()}원</p>
+										</li>
+									);
+								})
+							)}
+						</ul>
+						<p className="bottom__p">{`총 ${sumOfIncome().toLocaleString()}원`}</p>
+					</div>
+				</BookContainer>
 			</Container>
 			<Modal
 				state={isOpen}
@@ -134,6 +238,7 @@ const DiaryDetail = () => {
 };
 
 const Container = styled.div`
+	width: 100%;
 	display: flex;
 	height: 90vh;
 	overflow: scroll;
@@ -237,12 +342,127 @@ const Quill = styled(QuillContainer)`
 	.ql-toolbar {
 		display: none;
 	}
+
+	.ql-editor {
+		padding: 12px 2rem;
+	}
 `;
 
 const BookContainer = styled.div`
 	width: 30%;
 	height: 50vh;
 	margin-top: 5rem;
+	padding: 2rem;
+
+	> h3 {
+		font-size: 1.4rem;
+		color: #464656;
+		text-align: center;
+	}
+
+	.outgo__lists {
+		width: 100%;
+		height: 20rem;
+		border-radius: 8px;
+		margin-top: 1rem;
+		border: 1px solid #c5c5c5;
+		border-top: none;
+		> h5 {
+			padding: 0.5rem 0;
+			border-radius: 8px 8px 0px 0px;
+			font-size: 1.2rem;
+			text-align: center;
+			color: white;
+			background: ${(props) => props.theme.COLORS.LIGHT_RED};
+		}
+
+		.lists__header {
+			display: flex;
+			gap: 2.5rem;
+			padding: 0.5rem 1.5rem;
+
+			> p {
+				font-size: 1.2rem;
+				font-weight: 600;
+				text-decoration: underline;
+
+				&:nth-child(2) {
+					width: 10rem;
+				}
+			}
+		}
+
+		.lists {
+			border-bottom: 1px solid #c5c5c5;
+			height: 13rem;
+			overflow-y: scroll;
+			.null__p {
+				display: flex;
+				margin-top: 5rem;
+				justify-content: center;
+				align-items: center;
+				font-size: 1.2rem;
+				color: gray;
+			}
+
+			.list {
+				display: flex;
+				gap: 2.5rem;
+				padding: 0.7rem 1.5rem;
+
+				> p {
+					font-size: 1.2rem;
+					&:first-child {
+						width: 4rem;
+					}
+				}
+
+				@keyframes scroll {
+					0% {
+						transform: translateX(25%);
+					}
+					100% {
+						transform: translateX(-70%);
+					}
+				}
+
+				.over__div {
+					width: 10rem;
+					overflow: hidden;
+
+					> p {
+						font-size: 1.2rem;
+						animation: 5s linear 1s infinite normal none running scroll;
+						animation-delay: 2s;
+						white-space: nowrap;
+					}
+				}
+
+				.under__div {
+					width: 10rem;
+
+					> p {
+						font-size: 1.2rem;
+						white-space: nowrap;
+					}
+				}
+			}
+		}
+
+		.bottom__p {
+			font-size: 1.2rem;
+			display: flex;
+			justify-content: flex-end;
+			margin-right: 2rem;
+			margin-top: 0.5rem;
+		}
+	}
+
+	.income {
+		> h5 {
+			background: ${(props) => props.theme.COLORS.LIGHT_BLUE};
+		}
+	}
 `;
 
 export default DiaryDetail;
