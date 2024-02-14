ce3fc37b4227eaede2ab19d5d5ef04d3fc8fafc5
YujunSun0 <cons202@gmail.com>
2024-02-14 14:46:37 +0900

    style : toast 폰트 크기 수정

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -48,7 +48,7 @@ function App() {
           <Route path={"/findPassword"} element={<PasswordFind />} />
         </Route>
       </Routes>
-      <ToastContainer />
+      <ToastContainer style={{ fontSize: "1.4rem" }} />
     </>
   );
 }
