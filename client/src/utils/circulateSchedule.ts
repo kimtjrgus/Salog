8eeb86585c6a4cd795562a200f480c0d12ccab41
YujunSun0 <cons202@gmail.com>
2024-02-22 18:46:50 +0900

    feat : 금융 일정이 지난 항목 예외처리

--- client/src/utils/circulateSchedule.ts
+++ client/src/utils/circulateSchedule.ts
@@ -2,6 +2,11 @@ const circulateSchedule = (dateString:string) => {
   const currentDate = new Date(); // 현재 날짜와 시간을 가져옵니다.
   const targetDate = new Date(dateString); // 비교할 대상 날짜를 생성합니다.
 
+   // 대상 날짜가 현재 날짜 이전인 경우 false를 반환합니다.
+  if (targetDate < currentDate) {
+    return false;
+  }
+
   // 대상 날짜와 현재 날짜의 차이를 계산합니다.
   const differenceInTime = targetDate.getTime() - currentDate.getTime();
   // 1일은 24시간이고, 1시간은 60분, 1분은 60초, 1초는 1000밀리초
