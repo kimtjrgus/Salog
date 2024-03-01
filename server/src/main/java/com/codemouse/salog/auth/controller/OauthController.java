b152cbe9264a952754d2a3183863cc93d672ade8
jerry <021skyfall@gmail.com>
2024-03-01 17:35:20 +0900

    feat : 클라이언트가 핸들링 가능하게 Oauth 로그인 이후 토큰 핸들링

--- server/src/main/java/com/codemouse/salog/auth/controller/OauthController.java
+++ server/src/main/java/com/codemouse/salog/auth/controller/OauthController.java
@@ -1,10 +1,8 @@
 package com.codemouse.salog.auth.controller;
 
 import com.codemouse.salog.auth.service.OauthService;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -13,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
 import java.util.Map;
 
 @RestController
@@ -24,16 +23,17 @@ public class OauthController {
     private final OauthService oauthService;
 
     @GetMapping("/login/oauth2/code/google")
-    public ResponseEntity<?> googleOauth2Callback(@RequestParam("code") String authCode) {
+    public ResponseEntity<Map<String, String>> googleOauth2Callback(@RequestParam("code") String authCode, HttpServletResponse response) {
         try {
             String accessToken = oauthService.getAccessToken(authCode);
             String email = oauthService.getUserEmail(accessToken); // 액세스 토큰으로 이메일 정보 가져오기
 
             Map<String, String> tokens = oauthService.oauthUserHandler(email); // JWT 생성
 
+            response.sendRedirect("http://www.salog.kro.kr/oauthGoogle");
             return ResponseEntity.ok().body(tokens); // JWT 반환
         } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to get JWT");
+            throw new RuntimeException("Failed to get JWT", e);
         }
     }
 }
