5bbf462ba80cc17ec43786f72bd303b9a9c0781a
jerry <021skyfall@gmail.com>
2024-03-01 18:06:14 +0900

    feat : 클라이언트가 핸들링 가능하게 Oauth 로그인 이후 토큰 핸들링 v.2

--- server/src/main/java/com/codemouse/salog/auth/controller/OauthController.java
+++ server/src/main/java/com/codemouse/salog/auth/controller/OauthController.java
@@ -11,7 +11,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.net.URLEncoder;
 import java.util.Map;
 
 @RestController
@@ -23,15 +25,18 @@ public class OauthController {
     private final OauthService oauthService;
 
     @GetMapping("/login/oauth2/code/google")
-    public ResponseEntity<Map<String, String>> googleOauth2Callback(@RequestParam("code") String authCode, HttpServletResponse response) {
+    public void googleOauth2Callback(@RequestParam("code") String authCode, HttpServletResponse response) {
         try {
             String accessToken = oauthService.getAccessToken(authCode);
             String email = oauthService.getUserEmail(accessToken); // 액세스 토큰으로 이메일 정보 가져오기
 
             Map<String, String> tokens = oauthService.oauthUserHandler(email); // JWT 생성
 
-            response.sendRedirect("http://www.salog.kro.kr/oauthGoogle");
-            return ResponseEntity.ok().body(tokens); // JWT 반환
+            // 토큰을 쿼리 파라미터로 추가합니다.
+            String redirectUrl = "http://www.salog.kro.kr/oauthGoogle?accessToken=" + URLEncoder.encode(tokens.get("accessToken"), "UTF-8")
+                    + "&refreshToken=" + URLEncoder.encode(tokens.get("refreshToken"), "UTF-8");
+
+            response.sendRedirect(redirectUrl);
         } catch (Exception e) {
             throw new RuntimeException("Failed to get JWT", e);
         }
