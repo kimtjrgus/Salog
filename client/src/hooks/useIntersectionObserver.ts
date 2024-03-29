d49cc12897566bc6d36abfc94bb50dd39cf6f9c5
YujunSun0 <cons202@gmail.com>
2024-03-29 16:11:10 +0900

    docs : 주요 기능 주석 추가

--- client/src/hooks/useIntersectionObserver.ts
+++ client/src/hooks/useIntersectionObserver.ts
@@ -1,10 +1,12 @@
 import { useRef } from "react";
 
 export default function useIntersectionObserver(callback: () => void) {
+	// 컴포넌트가 리렌더링될 때마다 IntersectionObserver 인스턴스가 재생성 되는 것 방지
 	const observer = useRef(
 		new IntersectionObserver(
 			(entries: IntersectionObserverEntry[]) => {
 				entries.forEach((entry) => {
+					// 타겟 요소가 루트 요소와 교차하면 콜백 실행
 					if (entry.isIntersecting) {
 						callback();
 					}
@@ -15,7 +17,7 @@ export default function useIntersectionObserver(callback: () => void) {
 	);
 
 	// Element는 DOM 요소를 나타내는 타입임
-	const observe = (element: Element) => {
+	const observe = (element: Element) => {		
 		observer.current.observe(element);
 	};
 
