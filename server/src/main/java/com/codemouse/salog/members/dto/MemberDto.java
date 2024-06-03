1f4384c2570232071b5d34f2fcb6e5c36b976f46
jerry <021skyfall@gmail.com>
2024-06-03 18:44:04 +0900

    fix : MemberDto에 테스트 환경을 위해 @Setter 추가

--- server/src/main/java/com/codemouse/salog/members/dto/MemberDto.java
+++ server/src/main/java/com/codemouse/salog/members/dto/MemberDto.java
@@ -3,6 +3,7 @@ package com.codemouse.salog.members.dto;
 import com.codemouse.salog.tags.ledgerTags.dto.LedgerTagDto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.validation.constraints.NotNull;
@@ -12,7 +13,9 @@ import java.util.List;
 
 public class MemberDto {
     @AllArgsConstructor
+    @NoArgsConstructor
     @Getter
+    @Setter
     public static class Post {
         @NotNull
         @Pattern(regexp = "^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}$", message = "유효한 이메일 주소를 입력해주세요.")
@@ -25,14 +28,18 @@ public class MemberDto {
     }
 
     @AllArgsConstructor
+    @NoArgsConstructor
     @Getter
+    @Setter
     public static class Patch {
         private boolean emailAlarm;
         private boolean homeAlarm;
     }
 
     @AllArgsConstructor
+    @NoArgsConstructor
     @Getter
+    @Setter
     public static class PatchPassword {
         @NotNull
         @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[$@$!%*#?&])[A-Za-z\\d$@$!%*#?&]{8,}$", message = "유효한 비밀번호를 입력해주세요.")
@@ -42,6 +49,7 @@ public class MemberDto {
         private String newPassword;
     }
 
+    @NoArgsConstructor
     @Getter
     @Setter
     public static class Response {
